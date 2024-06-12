59dfd24002f564b135559976a5f324090325def2
Michael Orji <Orjimichael4886@gmail.com>
2024-06-12 08:27:11 +0100

    Update app.ts

--- app.ts
+++ app.ts
@@ -42,7 +42,7 @@ function telegramWelcomeCommand(bot: Bot) {
 
   bot.on('callback_query:game_short_name', async (ctx) => {
     console.log('Game event button called');
-    const usernameCtx = ctx.from.username ?? ctx.from.first_name;
+    const usernameCtx = ctx.from.username ?? `${ctx.from.first_name}${ctx.from.id}`;
 
     await ctx.answerCallbackQuery({
       url: `https://telegram-game-bot-frontend.onrender.com?username=${usernameCtx}`,
