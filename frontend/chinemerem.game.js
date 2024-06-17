cf8945da27f46b29aee0a8315088471dd00002af
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-06-17 19:29:39 +0100

    Restore original frontend code

--- frontend/chinemerem.game.js
+++ frontend/chinemerem.game.js
@@ -1,6 +1,6 @@
-const gameArea = document.getElementById('game-area');
-const message = document.getElementById('message');
-const timerElement = document.getElementById('timer');
+const gameArea = document.getElementById("game-area");
+const message = document.getElementById("message");
+const timerElement = document.getElementById("timer");
 const blinkInterval = 500; // Blinking interval in milliseconds
 let blinkIntervalId;
 handleMissingUsername();
@@ -13,23 +13,27 @@ let clicks = 0;
 // Function to retrieve username from URL (optional error handling)
 function getUsernameFromUrl() {
   const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get('username') || ''; // Return empty string if username not found
+  return urlParams.get("username") || ""; // Return empty string if username not found
 }
 
 // Function to check for missing username and redirect if needed
 function handleMissingUsername() {
   const username = getUsernameFromUrl();
-  if (!username) window.location = 'error.html';
+  if (!username) window.location = "error.html";
 }
 
 function handleClick() {
   clicks++;
   gameArea.textContent = clicks; // Update displayed click count
-
-  // Add animation styles for swelling on click
-  gameArea.classList.add('swell');
+  gameArea.classList.add("click-effect");
   setTimeout(() => {
-    gameArea.classList.remove('swell');
+    gameArea.classList.remove("click-effect");
+  }, 100);
+  // Add animation styles for swelling on click
+  gameArea.classList.add("swell");
+  // Match the transition duration
+  setTimeout(() => {
+    gameArea.classList.remove("swell");
   }, 200); // Remove animation class after 200ms (adjust duration as needed)
 }
 
@@ -37,14 +41,15 @@ function handleGameOver() {
   clearTimeout(timer);
   message.textContent = `Game Over! Your score: ${clicks}`;
   uploadScore(clicks); // Upload score
-  gameArea.removeEventListener('click', handleClick);
+  gameArea.removeEventListener("click", handleClick);
   startBlinking();
 }
 
 function startGame() {
+  document.getElementById("trigger-btn").style.display = "none";
   stopBlinking();
-  gameArea.addEventListener('click', handleClick);
-  message.textContent = 'Click as fast as you can!';
+  gameArea.addEventListener("click", handleClick);
+  message.textContent = "Tap as fast as you can!";
   clicks = 0; // Reset click count
   gameArea.textContent = clicks; // Update displayed click count initially
 
@@ -64,61 +69,61 @@ function updateTimer() {
   const seconds = remainingTime % 30 || 30; // Ensure seconds are displayed even at 0
   timerElement.textContent = `Time Remaining: ${seconds
     .toString()
-    .padStart(2, '0')}`;
+    .padStart(2, "0")}`;
 }
 
 async function uploadScore(score) {
   // Replace with your actual API URL and logic
   try {
     const response = await fetch(
-      'https://telegram-game-bot-tbdr.onrender.com/new-score',
+      "https://telegram-game-bot-tbdr.onrender.com/new-score",
       // 'http://localhost:5000/new-score',
       {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username: getUsernameFromUrl(), clicks: score }),
       }
     );
     const data = await response.json();
-    console.log('Score uploaded:', data);
+    console.log("Score uploaded:", data);
     updateLeaderBoard();
     // Handle successful upload, e.g., display confirmation message
   } catch (error) {
-    console.error('Error uploading score:', error);
+    console.error("Error uploading score:", error);
     // Handle upload error, e.g., display error message to user
   }
 }
 
 async function updateLeaderBoard() {
-  const leaderboardList = document.getElementById('leaderboard-list');
+  const leaderboardList = document.getElementById("leaderboard-list");
 
   try {
     const response = await fetch(
-      'https://telegram-game-bot-tbdr.onrender.com/leaderboard'
+      "https://telegram-game-bot-tbdr.onrender.com/leaderboard"
       // 'http://localhost:5000/leaderboard',
     );
     const data = await response.json();
 
     // Clear any existing list items
-    leaderboardList.innerHTML = '';
+    leaderboardList.innerHTML = "";
 
     // Loop through the data and create list items
     data.forEach((player) => {
-      const listItem = document.createElement('li');
+      const listItem = document.createElement("li");
       listItem.innerHTML = `<b><span class="user">${player.username}</span></b> | <span class="score">${player.clicks}</span> clicks`;
       leaderboardList.appendChild(listItem);
     });
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
     // Handle error gracefully, e.g., display an error message to the user
   }
 }
 
 function blink() {
   isVisible = !isVisible;
-  gameArea.style.visibility = isVisible ? 'visible' : 'hidden';
+  gameArea.style.visibility = isVisible ? "visible" : "hidden";
 }
 
 function startBlinking() {
@@ -126,6 +131,6 @@ function startBlinking() {
 }
 
 function stopBlinking() {
-  gameArea.style.visibility = 'visible';
+  gameArea.style.visibility = "visible";
   clearInterval(blinkIntervalId);
 }
